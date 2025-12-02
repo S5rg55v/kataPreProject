33f7877a26c1f7250d5f8571ddd87888d77d30b5
architect1732 <154232673+S5rg55v@users.noreply.github.com>
2025-12-02 23:55:36 +0200

    Update Util.java

--- PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/jdbcconnect";
     public static final String USER = "root";
     public static final String PASSWORD = "root";
 
-    public static Connection getConnection() {
+    public Connection getConnection() {
         Connection cnnct = null;
         try {
             Class.forName(DRIVER);
